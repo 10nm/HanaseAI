2ec3f8151e687d4d0f3b814e86cd02388d39edc3
10nm <10nm@protonmail.com>
2024-02-12 01:01:52 +0900

    api送れるように

--- hanaseai.py
+++ hanaseai.py
@@ -1,11 +1,18 @@
 import discord
+import openai
 import os
+
+openai.api_key = "EMPTY"
+openai.base_url = "http://localhost:8000/v1/"
+model = "vicuna-7b-v1.5"
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
-
+ 
     async def on_message(self, message):
+        ct = message.content
+
         if message.author == self.user:
             return
         
@@ -19,10 +26,22 @@ class MyClient(discord.Client):
         
         if message.content == '!leave':
             await message.guild.voice_client.disconnect()
-            
-            
+        
+        if ct.startswith('.') == True:
+            prompt = (message.content)
+            prompt = prompt.lstrip('.')
+            completion = await nlp(prompt)
+            await message.channel.send(completion.choices[0].text)
 
-token = os.environ['TOKEN']
+async def nlp(prompt):
+    response= openai.completions.create(
+        model=model, 
+        prompt=prompt, 
+        max_tokens=64,
+    )
+    return response
+
+token = os.environ.get("TOKEN")
 print(token)
 
 client = MyClient(intents=discord.Intents.all())
